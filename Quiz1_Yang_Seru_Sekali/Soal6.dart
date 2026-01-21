8517a8ca329edf6368a6883c3e9ce3e60a9a14d3
Gede Hadrian <igedehadrian45@gmail.com>
2026-01-21 14:29:24 +0700

    soal6

---
+++ Quiz1_Yang_Seru_Sekali/Soal6.dart
@@ -0,0 +1,25 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main() {
+  Account a = Account();
+  a.depo(100);
+  a.wd(25);
+}
+
+class Account {
+  double saldo = 0;
+  void depo(nilai) {
+    saldo += nilai;
+    print("saldo sekarang $saldo ");
+  }
+
+  void wd(nilai) {
+    if (saldo - nilai < 0) {
+      print("Saldo Kurang");
+    } else
+      saldo = saldo - nilai;
+
+    print("saldo sekarang $saldo ");
+  }
+}
