d9009e9a07247b39df0840baf010cb63330433f3
Gede Hadrian <igedehadrian45@gmail.com>
2026-01-21 13:25:25 +0700

    Soal 1 Commit For Loop Even Number

---
+++ Quiz1_Yang_Seru_Sekali/Soal1.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<int> a = [1, 4, 9, 16, 25, 36, 49, 63, 81, 100];
+  List<int> b = [];
+
+  for (int i in a) {
+    if (i % 2 == 0) {
+      b.add(i);
+    }
+  }
+
+  print(b);
+}
