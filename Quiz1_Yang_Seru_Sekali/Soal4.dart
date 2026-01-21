5789be62b127e4d882887c9831c8658446fcf2fd
Gede Hadrian <igedehadrian45@gmail.com>
2026-01-21 14:56:55 +0700

    rev

--- Quiz1_Yang_Seru_Sekali/Soal4.dart
+++ Quiz1_Yang_Seru_Sekali/Soal4.dart
@@ -5,16 +5,23 @@ void main() {
 }
 
 void prima() {
-  int? angka = stdin.readByteSync();
+  stdout.write("Masukkan angka: ");
+  String? angka = stdin.readLineSync();
+  int angkaInt = int.tryParse(angka ?? "") ?? 0;
   var condition = true;
 
-  for (int i = 0; i <= angka; i++) {
-    if (angka % i == 0 && i != angka) {
-      condition = false;
+  if (angkaInt <= 1) {
+    condition = false;
+  } else {
+    for (int i = 2; i < angkaInt; i++) {
+      if (angkaInt % i == 0) {
+        condition = false;
+        break;
+      }
     }
   }
 
-  if (condition == false) {
+  if (condition) {
     print("Prima");
   } else {
     print("Bukan Prima");
